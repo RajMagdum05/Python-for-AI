74c8e490278fb59ff5c6e9f145033b33136feb8c
RajMagdum05 <rajmagdum6844@gmail.com>
2026-07-28 14:19:15 +0530

    added all files

---
+++ notes.py
@@ -0,0 +1,24 @@
+"""
+# Packages - pre written code that we can use
+# requests - for downloading web pages
+# pandas - for working with data
+# openai - for using AI models
+#  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
